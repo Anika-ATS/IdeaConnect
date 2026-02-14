c2f17af072c05b55b67ac203da71f641d3a601f3
Anika-ATS <shuchi99anika@gmail.com>
2026-02-14 13:57:23 +0600

    nav and footer

--- src/layouts/RootLayouts.jsx
+++ src/layouts/RootLayouts.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
-//import Footer from '../pages/Shared/Footer/Footer';
-//import NavBar from '../pages/Shared/NavBar/NavBar';
+import Navbar from '../pages/Shared/Navbar/Navbar';
+import Footer from '../pages/Shared/Footer/Footer';
 
 const RootLayouts = () => {
     return (
         <div className='max-w-7xl mx-auto'>
-            
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
           
         </div>
     );
